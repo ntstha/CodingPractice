045eca900e1767c70435491e22c6bf977c3f624d
Nishit Shrestha <nishitshrestha@gmail.com>
2019-04-09 11:34:13 -0700

    Update CountUnivalSubTree.java

--- CountUnivalSubTree.java
+++ CountUnivalSubTree.java
@@ -13,3 +13,34 @@ For example, the following tree has 5 unival subtrees:
   / \
  1   1
 **/
+
+public static int countUnival(TreeNode root){
+        if(root==null){
+            return 0;
+        }
+        int count =0;
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.add(root);
+        while(!queue.isEmpty()){
+            TreeNode node = queue.remove();
+            boolean isLeft = true;
+            boolean isRight = true;
+            if(node.left!=null){
+                if(node.left.val != node.val){
+                    isLeft = false;
+                }
+                queue.add(node.left);
+            }
+            if(node.right!=null){
+                if(node.right.val != node.val){
+                    isRight = false;
+                }
+                queue.add(node.right);
+            }
+            
+            if(isLeft && isRight){
+                count = count+1;
+            }
+        }
+        return count;
+    }
